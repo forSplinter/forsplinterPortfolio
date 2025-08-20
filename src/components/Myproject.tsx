934d30caf8e53b3c39c0c08e1bc3b154874fe8d1
forsplinter <mugangamathys24@gmail.com>
2025-08-20 15:13:56 +0200

    refacto button Navbar and add button on MyProject

--- src/components/Myproject.tsx
+++ src/components/Myproject.tsx
@@ -3,8 +3,7 @@ import React, { useEffect } from "react";
 import GridLayout from "./GridLayout";
 import { projects } from "../data";
 import ProjectCard from "./card/ProjectCard";
-import styles from './card/ProjectCard.module.css'
-import {motion, useScroll} from 'framer-motion';
+import { motion, useScroll } from 'framer-motion';
 import { useRef } from "react";
 
 const MyProject = () => {
@@ -22,42 +21,39 @@ const MyProject = () => {
                 <span className="text-[#1e1e1e] tracking-[-5.53px]">.</span>
             </h1>
 
-            {/* Grid background */}
             <div className="absolute top-[100px] left-[120px] right-[120px] px-[120px] h-[700px] -z-10">
                 <GridLayout />
             </div>
 
-            {/* Cards list */}
-            <div className={styles.cardsWrapper}>
+            <div>
                 {projects.map((project, index) => {
                     const targetScale = 1 - (projects.length - index) * 0.05;
-                    return <ProjectCard key={index} i={index} {...project} progress={scrollYProgress} range={[index * 0.25, 1]}  targetScale={targetScale}/>
+                    return <ProjectCard key={index} i={index} {...project} progress={scrollYProgress} range={[index * 0.25, 1]} targetScale={targetScale} />
                 })}
+                <div className="mt-27 flex justify-center">
+                    <button
+                        className="
+                                        font-plus-jakarta-sans
+                                        text-[20px]
+                                        inline-flex justify-center items-center relative whitespace-nowrap
+                                        rounded-[15px] border-3 border-[#56adcb] h-[36px] px-4 gap-x-[10px]
+                                        bg-transparent text-[#56adcb] font-bold
+                                        hover:bg-black hover:text-white hover:border-transparent
+                                        transition-colors duration-200 ease-in-out
+                                        focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 ring-offset-background
+                                        disabled:pointer-events-none disabled:opacity-50
+                                        [&_svg]:pointer-events-none [&_svg]:w-4 [&_svg]:h-4 [&_svg]:shrink-0
+                                        group
+                                        "
+                        aria-label="Get started"
+                    >
+                        See more
+                    </button>
+                </div>
+
+                </div>
             </div>
-            <div>
-                <h1 className="ml-[120px] mt-[20px] font-plus-jakarta-sans font-extrabold text-transparent text-8xl tracking-[-5.76px] leading-[normal] whitespace-nowrap">
-                    <span className="text-[#1e1e1e] tracking-[-5.53px]">My</span>
-                    <span className="text-[#56adcb] tracking-[-5.53px] ml-3">project</span>
-                    <span className="text-[#1e1e1e] tracking-[-5.53px]">.</span>
-                </h1>
-                <h1 className="ml-[120px] mt-[20px] font-plus-jakarta-sans font-extrabold text-transparent text-8xl tracking-[-5.76px] leading-[normal] whitespace-nowrap">
-                    <span className="text-[#1e1e1e] tracking-[-5.53px]">My</span>
-                    <span className="text-[#56adcb] tracking-[-5.53px] ml-3">project</span>
-                    <span className="text-[#1e1e1e] tracking-[-5.53px]">.</span>
-                </h1>
-                <h1 className="ml-[120px] mt-[20px] font-plus-jakarta-sans font-extrabold text-transparent text-8xl tracking-[-5.76px] leading-[normal] whitespace-nowrap">
-                    <span className="text-[#1e1e1e] tracking-[-5.53px]">My</span>
-                    <span className="text-[#56adcb] tracking-[-5.53px] ml-3">project</span>
-                    <span className="text-[#1e1e1e] tracking-[-5.53px]">.</span>
-                </h1>
-                <h1 className="ml-[120px] mt-[20px] font-plus-jakarta-sans font-extrabold text-transparent text-8xl tracking-[-5.76px] leading-[normal] whitespace-nowrap">
-                    <span className="text-[#1e1e1e] tracking-[-5.53px]">My</span>
-                    <span className="text-[#56adcb] tracking-[-5.53px] ml-3">project</span>
-                    <span className="text-[#1e1e1e] tracking-[-5.53px]">.</span>
-                </h1>
-            </div>
-        </div>
-    );
+            );
 };
 
-export default MyProject;
+            export default MyProject;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,13 +8,13 @@ const Navbar = () => {
         <nav>
             <header className="fixed inset-x-0 top-0 z-[99] bg-[#ecf6ff] dark:bg-background">
                 <div className="w-full px-[120px] flex items-start justify-between pt-[32px] pb-4">
-                    {/* Logo */}
+
                     <p className="w-fit font-extrabold text-[32px] tracking-[-1.92px]">
                         <span className="text-[#56adcb] tracking-[-0.61px]">For</span>
                         <span className="text-[#1e1e1e] tracking-[-0.61px]">Splinter</span>
                     </p>
 
-                    {/* Menu gauche */}
+                    
                     <div className="flex flex-col justify-start gap-[3px]">
                         {["My projects", "About me", "Writings", "Photos&Artworks"].map((item, idx) => (
                             <div key={idx} className="font-bold text-[#56adcb] text-xl">
@@ -23,7 +23,7 @@ const Navbar = () => {
                         ))}
                     </div>
 
-                    {/* Menu droit */}
+                    
                     <div className="flex flex-col justify-start w-[120px] gap-[3px]">
                         {["LinkedIn", "Github", "Apple Music"].map((item, idx) => (
                             <div key={idx} className="font-bold text-[#56adcb] text-xl">
@@ -31,14 +31,13 @@ const Navbar = () => {
                             </div>
                         ))}
                     </div>
-
-                    {/* Bouton "Get started" */}
+                    
                     <button
                         className="
                         font-plus-jakarta-sans
                         text-[20px]
                         inline-flex justify-center items-center relative whitespace-nowrap
-                        rounded-[42px] border-3 border-[#56adcb] h-[36px] px-4 gap-x-[10px]
+                        rounded-[15px] border-3 border-[#56adcb] h-[36px] px-4 gap-x-[10px]
                         bg-transparent text-[#56adcb] font-bold
                         hover:bg-black hover:text-white hover:border-transparent
                         transition-colors duration-200 ease-in-out
