07cc45183e5340d1ff730de2b587b59ae4f25f12
forsplinter <mugangamathys24@gmail.com>
2025-08-20 15:37:13 +0200

    refactor layout structure in Home component for better readability

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,21 @@
 import Header from "@/components/Header";
 import Myproject from "@/components/Myproject";
 import ScrollTable from "@/components/ScrollTable";
-import {projects} from '../data';
+import { projects } from '../data';
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
-      <Header />
-      <ScrollTable />
-      <Myproject />
-    </>
+    <div>
+      <section id="header"  className="mt-32">
+        <Header />
+      </section>
+      <section className="mt-10">
+        <ScrollTable />
+      </section>
+      <section id="my-projects" className="mt-32">
+        <Myproject />
+      </section>
+    </div>
   );
 }
