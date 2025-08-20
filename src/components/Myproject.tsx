d47c661cde0decdfc9bf665c8adc139c0ff9458a
forsplinter <mugangamathys24@gmail.com>
2025-08-20 15:02:01 +0200

    end of the scroll animation

--- src/components/Myproject.tsx
+++ src/components/Myproject.tsx
@@ -1,30 +1,63 @@
-import React from "react";
+'use client';
+import React, { useEffect } from "react";
 import GridLayout from "./GridLayout";
 import { projects } from "../data";
 import ProjectCard from "./card/ProjectCard";
+import styles from './card/ProjectCard.module.css'
+import {motion, useScroll} from 'framer-motion';
+import { useRef } from "react";
 
 const MyProject = () => {
-  return (
-    <div className="relative">
-      <h1 className="ml-[120px] mt-[20px] font-plus-jakarta-sans font-extrabold text-transparent text-8xl tracking-[-5.76px] leading-[normal] whitespace-nowrap">
-        <span className="text-[#1e1e1e] tracking-[-5.53px]">My</span>
-        <span className="text-[#56adcb] tracking-[-5.53px] ml-3">project</span>
-        <span className="text-[#1e1e1e] tracking-[-5.53px]">.</span>
-      </h1>
+    const container = useRef(null);
+    const { scrollYProgress } = useScroll({
+        target: container.current ?? undefined,
+        offset: ["start start", "end end"]
+    })
 
-      {/* Grid background */}
-      <div className="absolute top-[100px] left-[120px] right-[120px] px-[120px] h-[700px] -z-10">
-        <GridLayout />
-      </div>
+    return (
+        <div className="relative">
+            <h1 className="ml-[120px] mt-[20px] font-plus-jakarta-sans font-extrabold text-transparent text-8xl tracking-[-5.76px] leading-[normal] whitespace-nowrap">
+                <span className="text-[#1e1e1e] tracking-[-5.53px]">My</span>
+                <span className="text-[#56adcb] tracking-[-5.53px] ml-3">project</span>
+                <span className="text-[#1e1e1e] tracking-[-5.53px]">.</span>
+            </h1>
 
-      {/* Cards list */}
-      <div>
-        {projects.map((project, index) => (
-          <ProjectCard key={index} {...project} />
-        ))}
-      </div>
-    </div>
-  );
+            {/* Grid background */}
+            <div className="absolute top-[100px] left-[120px] right-[120px] px-[120px] h-[700px] -z-10">
+                <GridLayout />
+            </div>
+
+            {/* Cards list */}
+            <div className={styles.cardsWrapper}>
+                {projects.map((project, index) => {
+                    const targetScale = 1 - (projects.length - index) * 0.05;
+                    return <ProjectCard key={index} i={index} {...project} progress={scrollYProgress} range={[index * 0.25, 1]}  targetScale={targetScale}/>
+                })}
+            </div>
+            <div>
+                <h1 className="ml-[120px] mt-[20px] font-plus-jakarta-sans font-extrabold text-transparent text-8xl tracking-[-5.76px] leading-[normal] whitespace-nowrap">
+                    <span className="text-[#1e1e1e] tracking-[-5.53px]">My</span>
+                    <span className="text-[#56adcb] tracking-[-5.53px] ml-3">project</span>
+                    <span className="text-[#1e1e1e] tracking-[-5.53px]">.</span>
+                </h1>
+                <h1 className="ml-[120px] mt-[20px] font-plus-jakarta-sans font-extrabold text-transparent text-8xl tracking-[-5.76px] leading-[normal] whitespace-nowrap">
+                    <span className="text-[#1e1e1e] tracking-[-5.53px]">My</span>
+                    <span className="text-[#56adcb] tracking-[-5.53px] ml-3">project</span>
+                    <span className="text-[#1e1e1e] tracking-[-5.53px]">.</span>
+                </h1>
+                <h1 className="ml-[120px] mt-[20px] font-plus-jakarta-sans font-extrabold text-transparent text-8xl tracking-[-5.76px] leading-[normal] whitespace-nowrap">
+                    <span className="text-[#1e1e1e] tracking-[-5.53px]">My</span>
+                    <span className="text-[#56adcb] tracking-[-5.53px] ml-3">project</span>
+                    <span className="text-[#1e1e1e] tracking-[-5.53px]">.</span>
+                </h1>
+                <h1 className="ml-[120px] mt-[20px] font-plus-jakarta-sans font-extrabold text-transparent text-8xl tracking-[-5.76px] leading-[normal] whitespace-nowrap">
+                    <span className="text-[#1e1e1e] tracking-[-5.53px]">My</span>
+                    <span className="text-[#56adcb] tracking-[-5.53px] ml-3">project</span>
+                    <span className="text-[#1e1e1e] tracking-[-5.53px]">.</span>
+                </h1>
+            </div>
+        </div>
+    );
 };
 
 export default MyProject;
--- src/components/card/ProjectCard.tsx
+++ src/components/card/ProjectCard.tsx
@@ -1,28 +1,41 @@
 'use client';
-import styles from './ProjectCard.module.css'
-import React, { use, useRef } from 'react'
-import { motion, useScroll, useTransform } from 'framer-motion'
-import Image from 'next/image'
+import styles from './ProjectCard.module.css';
+import React, { useRef } from 'react';
+import { motion, useScroll, useTransform } from 'framer-motion';
 
-export default function ProjectCard({ title, description, src, link, color }) {
+export default function ProjectCard({ i, color, gap = 50, targetScale = 0.9 }) {
     const container = useRef(null);
+
     const { scrollYProgress } = useScroll({
         target: container,
-        offset: ["start end", "start start"]
+        offset: ["start end", "end start"]
     });
-    const scale = useTransform(scrollYProgress, [0, 1], [2, 1]);
+    
+    const scale = useTransform(
+        scrollYProgress,
+        [1 - gap / 90, 1],  
+        [1, targetScale]
+    );
+    
+    const yOffset = useTransform(
+        scrollYProgress,
+        [0, 1],
+        [0, -i * 20] // plus i est petit, moins la carte bouge
+    );
+
     return (
         <div ref={container} className={styles.cardContainer}>
-            <div style={{ backgroundColor: color }} className={styles.card}>
-                <div className={styles.imageContainer}>
-                    <motion.div style={{ scale }} className={styles.inner}>
-                        <Image fill src={src} alt={title} />
-                    </motion.div>
-
-                </div>
-
-            </div>
+            <motion.div
+                style={{
+                    backgroundColor: color,
+                    top: `calc(-5% + ${i * gap}px)`,
+                    scale,
+                    y: yOffset
+                }}
+                className={styles.card}
+            >
+                <div className={styles.imageContainer}></div>
+            </motion.div>
         </div>
-
-    )
+    );
 }
